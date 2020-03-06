4547a83b2b4e3f47a01f6186a76ce02725152ee2
steve <steve0639@cchntek.com>
2020-03-06 11:12:16 +0800

    Updates

---
+++ precache-manifest.15c4c0d26162cf309eda62de00805360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c78aa2c1f5f613dc47b7ab1357ebaea",
+    "url": "/react-push-github/index.html"
+  },
+  {
+    "revision": "25b3b84f5d9f974b518a",
+    "url": "/react-push-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3d7e0acde0f367a0563",
+    "url": "/react-push-github/static/js/2.ea2163a0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-push-github/static/js/2.ea2163a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b3b84f5d9f974b518a",
+    "url": "/react-push-github/static/js/main.686f077e.chunk.js"
+  },
+  {
+    "revision": "c4029a81d618a6cabc30",
+    "url": "/react-push-github/static/js/runtime-main.4c801fe9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-push-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/git@github.com:steve19890707/react-push-github.git/precache-manifest.f392e5f70c2830abc4f721becbe26f43.js"
+  "/react-push-github/precache-manifest.15c4c0d26162cf309eda62de00805360.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/git@github.com:steve19890707/react-push-github.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-push-github/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
